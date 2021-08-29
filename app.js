7ffdd48e66f577aabf52d8903835c9b4fea8cb83
develad <develadb@gmail.com>
2021-08-29 14:01:15 +0300

    :sparkles:initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const box = document.querySelector('.box');
+const controlIcon = document.querySelector('.control-icon');
+
+const getSong = async () => {
+  const res = await fetch('https://dagbareshet.herokuapp.com/kissfm');
+  const data = await res.json();
+  box.innerHTML = `
+            <h2>Now Playing:</h2>
+            <h1 id="song">${data.song}</h1>
+            <h3 id="singer">${data.singer}</h3>
+            <img id="songImg" src="${data.thumbnail}"/>
+            <a href="${data.videoLink}" target="_blank"><img src="./assets/youtubeBtn.png" id="youtube-icon"/></a>
+        `;
+};
+
+getSong();
+setInterval(getSong, 1000 * 60);
+
+let isPlaying = false;
+const radio = new Audio('https://online.kissfm.ua/KissFM_Digital_HD');
+
+controlIcon.addEventListener('click', () => {
+  if (!isPlaying) {
+    radio.load();
+    radio.play();
+    isPlaying = !isPlaying;
+    controlIcon.innerHTML = `<i class="far fa-stop-circle" id="play" ></i>`;
+    controlIcon.classList.add('spin');
+    equ.style.display = 'block';
+    setTimeout(() => {
+      controlIcon.classList.remove('spin');
+    }, 500);
+  } else {
+    radio.pause();
+    isPlaying = !isPlaying;
+    controlIcon.innerHTML = `<i class="far fa-play-circle" id="stop"></i>`;
+    controlIcon.classList.add('spin');
+    equ.style.display = 'none';
+    setTimeout(() => {
+      controlIcon.classList.remove('spin');
+    }, 500);
+  }
+});
