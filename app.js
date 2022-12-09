7ca2ff067d271df94e078d29262d706660d18d0c
develad <70572668+develad@users.noreply.github.com>
2022-12-09 23:52:08 +0200

    changing fetch URL end-point

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const controlIcon = document.querySelector('.control-icon');
 const videoLink = document.querySelector('.videoLink');
 
 const getSong = async () => {
-  const res = await fetch('https://dagbareshet.herokuapp.com/kissfm');
+  // const res = await fetch('https://dagbareshet.herokuapp.com/kissfm');
+  const res = await fetch('https://dark-gray-snail-ring.cyclic.app/kissfm');
   const data = await res.json();
   box.innerHTML = `
             <h2>Now Playing:</h2>
